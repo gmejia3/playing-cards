ad1f4d8db845c82fabb27f24e8c00352b000365d
gmejia3 <gemejiajr@gmail.com>
2022-09-27 13:21:13 -0700

    Deck Sorted.

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -47,7 +47,11 @@ public class Card implements Comparable<Card> {
 
   @Override
   public int compareTo(Card other) {
-    return 0; // TODO: 9/16/2022 Replace with comparison based on suit and rank. 
+    int comparison = suit.compareTo(other.suit);
+    if (comparison == 0) {
+      comparison = rank.compareTo(other.rank);
+    }
+    return comparison;
   }
 
 }
